79987ac5740e28bb576cbc0e19affe1d71f170eb
mmtakahashi <m.takahashi@gftd.works>
2021-09-09 19:27:25 +0900

    ADD : hamburger

--- js/normalize.js
+++ js/normalize.js
@@ -1,6 +1,6 @@
 // ハンバーガーメニュー押下時にアクティブに切り替える
 window.addEventListener('load',() => {
-    const humToggle = document.querySelector('.hum-toggle');
+    const humToggle = document.querySelector('.hum-act');
     document.addEventListener('click', () => {
         humToggle.classList.toggle('active');
         document.querySelector('.accordion').classList.toggle('active');
