b7d1447d1f63deeb2340f514268ff436990b8bbf
mmtakahashi <m.takahashi@gftd.works>
2021-09-10 15:40:07 +0900

    ADD : move hamburger

--- js/normalize.js
+++ js/normalize.js
@@ -1,8 +1,23 @@
 // ハンバーガーメニュー押下時にアクティブに切り替える
+// window.addEventListener('load',() => {
+//     const humToggle = document.querySelectorAll('.hum-act');
+//     humToggle.forEach(act => {
+//         act.addEventListener('click', () => {
+//             // console.log('Hallo');
+//             // humToggle.classList.toggle('active');
+//             document.querySelector('.accordion').classList.toggle('active');
+//         });
+//     });
+// });
+
 window.addEventListener('load',() => {
-    const humToggle = document.querySelector('.hum-act');
-    document.addEventListener('click', () => {
+    const humToggle = document.getElementById('hum-button');
+    humToggle.addEventListener('click',() => {
         humToggle.classList.toggle('active');
-        document.querySelector('.accordion').classList.toggle('active');
+        document.getElementById('lines__container').classList.toggle('active');
+        document.getElementById('buns-top').classList.toggle('active');
+        document.getElementById('patty').classList.toggle('active');
+        document.getElementById('buns-bottom').classList.toggle('active');
+        document.getElementById('accordion').classList.toggle('active');
     })
 });
